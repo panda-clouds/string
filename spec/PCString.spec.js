995a0a469a79ff6e69b4290227748be0fe4ace77
Marc Smith <mr.marcsmith@me.com>
2018-12-19 01:18:17 -0700

    renamed functions

--- spec/PCString.spec.js
+++ spec/PCString.spec.js
@@ -33,130 +33,130 @@ describe('PCString.isString', () => {
 	});
 });
 
-describe('PCString.stringWithWhitespacesRemoved', () => {
+describe('PCString.removeWhitespace', () => {
 
 	it('should replace spaces', () => {
 		const string = " A B  C   D    ";
-		const newString = PCString.stringWithWhitespacesRemoved(string);
+		const newString = PCString.removeWhitespace(string);
 		expect(newString).toBe("ABCD");
 	});
 
 	it('should replace tabs', () => {
 		const string = "	A	B		C			D				";
-		const newString = PCString.stringWithWhitespacesRemoved(string);
+		const newString = PCString.removeWhitespace(string);
 		expect(newString).toBe("ABCD");
 	});
 
 	it('should replace tabs', () => {
 		const string = "	A	B		C			D				";
-		const newString = PCString.stringWithWhitespacesRemoved(string);
+		const newString = PCString.removeWhitespace(string);
 		expect(newString).toBe("ABCD");
 	});
 
 	it('should replace new lines', () => {
 		const string = "\nA\nB\n\nC\n\n\nD\n\n\n\n";
-		const newString = PCString.stringWithWhitespacesRemoved(string);
+		const newString = PCString.removeWhitespace(string);
 		expect(newString).toBe("ABCD");
 	});
 
 	it('should handle null', () => {
 		const string = null;
-		const newString = PCString.stringWithWhitespacesRemoved(string);
+		const newString = PCString.removeWhitespace(string);
 		expect(newString).toBe('');
 	});
 
 
 });
 
-describe('PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers', () => {
+describe('PCString.domainSafeString', () => {
 
 	it('should replace underscores', () => {
 		const string = "_A_B__C___D____";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("abcd");
 	});
 
 
 	it('should replace spaces', () => {
 		const string = " A B  C   D    ";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("abcd");
 	});
 
 	it('should replace tabs', () => {
 		const string = "  A B   C     D       ";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("abcd");
 	});
 
 	it('should replace tabs', () => {
 		const string = "  A B   C     D       ";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("abcd");
 	});
 
 	it('should replace new lines', () => {
 		const string = "\nA\nB\n\nC\n\n\nD\n\n\n\n";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("abcd");
 	});
 
 
 	it('should replace new "My Cool Domain"', () => {
 		const string = "My Cool Domain";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("mycooldomain");
 	});
 
 	it('should leave hyphens alone', () => {
 		const string = "My-Cool-Domain";
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe("my-cool-domain");
 	});
 
 	it('should handle null', () => {
 		const string = null;
-		const newString = PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
+		const newString = PCString.domainSafeString(string);
 		expect(newString).toBe('');
 	});
 
 
 });
-describe('PCString.hasWhiteSpace', () => {
+describe('PCString.hasWhitespace', () => {
 
 	it('should detect only spaces', () => {
 		const string = " ";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(true);
 	});
 
 	it('should detect leading spaces', () => {
 		const string = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(true);
 	});
 
 	it('should detect trailing spaces', () => {
 		const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(true);
 	});
 
 	it('should detect single spaces', () => {
 		const string = "A BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(true);
 	});
 
 	it('should detect multiple spaces', () => {
 		const string = " A B  C   D    ";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(true);
 	});
 
 	it('should not false positive', () => {
 		const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-!@#$%^&*()";
-		const newString = PCString.hasWhiteSpace(string);
+		const newString = PCString.hasWhitespace(string);
 		expect(newString).toBe(false);
 	});
 });
--- src/PCString.js
+++ src/PCString.js
@@ -4,34 +4,25 @@ class PCString  {
 		//Empty Constructor
 	}
 
-	static stringWithWhitespacesRemoved(string){
-		if(!string) return '';
-		return string.replace(/\s+/g, '');
-	}
-
-	// wrapper
-	static domainSafeString(string){
-		return PCString.stringWithOnlyLowercaseLettersHyphensAndNumbers(string);
-	}
-
-	static stringWithOnlyLowercaseLettersHyphensAndNumbers(string) {
-		if(!string) return '';
-		return string.toLowerCase().replace(/[^a-z0-9\-]/g, '');
-		// removed case insensitivity (i) because we use toLowerCase() before
-		// return string.toLowerCase().replace(/[^a-z0-9\-]/gi, '');
-	}
-
 	static isString(o) {
 		if(o === null) return false;
 		return typeof o == "string" || (typeof o == "object" && o.constructor === String);
 	}
 
-	static hasWhiteSpace(s) {
+	static hasWhitespace(s) {
 		return s.indexOf(' ') >= 0;
 	}
+
+	static removeWhitespace(string){
+		if(!string) return '';
+		return string.replace(/\s+/g, '');
+	}
+
+	static domainSafeString(string){
+		if(!string) return '';
+		return string.toLowerCase().replace(/[^a-z0-9\-]/g, '');
+	}
+
 }
 
 module.exports = PCString;
-// function() {
-// 	PCString = PCString;
-// }
