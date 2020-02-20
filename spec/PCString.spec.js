8211877462467ce34d5732d7bb0c6880d0eab344
Marc Smith <mr.marcsmith@me.com>
2020-02-20 16:17:50 -0700

    1.0.0 (#5)
    * added repo info
    
    * added coverage
    
    * removed coverage
    
    * full coverage

--- spec/PCString.spec.js
+++ spec/PCString.spec.js
@@ -1,34 +1,52 @@
-const PCString = require("../src/PCString.js");
+const PCString = require('../src/PCString.js');
 
-describe('PCString.isString', () => {
-
+describe('test PCString.isString', () => {
 	it('returns false when given number', () => {
+		expect.assertions(1);
 		const string = 5;
 		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(false);
 	});
 
 	it('returns false when given a undefined', () => {
+		expect.assertions(1);
+		// eslint-disable-next-line no-undefined
 		const string = undefined;
 		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(false);
 	});
 
 	it('returns false when given a null', () => {
+		expect.assertions(1);
 		const string = null;
 		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(false);
 	});
 
 	it('returns false when given an object', () => {
+		expect.assertions(1);
 		const string = {};
 		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(false);
 	});
 
 	it('returns false when given an array', () => {
+		expect.assertions(1);
 		const string = [];
 		const boolResults = PCString.isString(string);
+
+		expect(boolResults).toBe(false);
+	});
+
+	it('returns false when given a Date', () => {
+		expect.assertions(1);
+		const string = new Date();
+		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(false);
 	});
 
@@ -39,103 +57,121 @@ describe('PCString.isString', () => {
 	});
 
 	it('returns true when given a string', () => {
-		const string = "hello";
+		expect.assertions(1);
+		const string = 'hello';
 		const boolResults = PCString.isString(string);
+
 		expect(boolResults).toBe(true);
 	});
 });
 
-describe('PCString.hasWhitespace', () => {
-
+describe('test PCString.hasWhitespace', () => {
 	it('should return false when null', () => {
+		expect.assertions(1);
 		const string = null;
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(false);
 	});
 
 	it('should detect only spaces', () => {
-		const string = " ";
+		expect.assertions(1);
+		const string = ' ';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(true);
 	});
 
 	it('should detect leading spaces', () => {
-		const string = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		expect.assertions(1);
+		const string = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(true);
 	});
 
 	it('should detect trailing spaces', () => {
-		const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ";
+		expect.assertions(1);
+		const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(true);
 	});
 
 	it('should detect single spaces', () => {
-		const string = "A BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		expect.assertions(1);
+		const string = 'A BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(true);
 	});
 
 	it('should detect multiple spaces', () => {
-		const string = " A B  C   D    ";
+		expect.assertions(1);
+		const string = ' A B  C   D    ';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(true);
 	});
 
 	it('should not false positive', () => {
-		const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-!@#$%^&*()";
+		expect.assertions(1);
+		const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-!@#$%^&*()';
 		const boolResults = PCString.hasWhitespace(string);
+
 		expect(boolResults).toBe(false);
 	});
 });
 
-describe('PCString.removeWhitespace', () => {
-
+describe('test PCString.removeWhitespace', () => {
 	it('should replace spaces', () => {
-		const string = " A B  C   D    ";
+		expect.assertions(1);
+		const string = ' A B  C   D    ';
 		const newString = PCString.removeWhitespace(string);
-		expect(newString).toBe("ABCD");
+
+		expect(newString).toBe('ABCD');
 	});
 
 	it('should replace tabs', () => {
-		const string = "	A	B		C			D				";
+		expect.assertions(1);
+		const string = '	A	B		C			D				';
 		const newString = PCString.removeWhitespace(string);
-		expect(newString).toBe("ABCD");
-	});
 
-	it('should replace tabs', () => {
-		const string = "	A	B		C			D				";
-		const newString = PCString.removeWhitespace(string);
-		expect(newString).toBe("ABCD");
+		expect(newString).toBe('ABCD');
 	});
 
 	it('should replace new lines', () => {
-		const string = "\nA\nB\n\nC\n\n\nD\n\n\n\n";
+		expect.assertions(1);
+		const string = '\nA\nB\n\nC\n\n\nD\n\n\n\n';
 		const newString = PCString.removeWhitespace(string);
-		expect(newString).toBe("ABCD");
+
+		expect(newString).toBe('ABCD');
 	});
 
 	it('should handle null', () => {
+		expect.assertions(1);
 		const string = null;
 		const newString = PCString.removeWhitespace(string);
+
 		expect(newString).toBe('');
 	});
 });
 
-describe('PCString.domainSafeString', () => {
-
+describe('test PCString.domainSafeString', () => {
 	it('should replace underscores', () => {
-		const string = "_A_B__C___D____";
+		expect.assertions(1);
+		const string = '_A_B__C___D____';
 		const newString = PCString.domainSafeString(string);
-		expect(newString).toBe("abcd");
+
+		expect(newString).toBe('abcd');
 	});
 
 
 	it('should replace spaces', () => {
-		const string = " A B  C   D    ";
+		expect.assertions(1);
+		const string = ' A B  C   D    ';
 		const newString = PCString.domainSafeString(string);
-		expect(newString).toBe("abcd");
+		expect(newString).toBe('abcd');
 	});
 
 	it('should replace tabs', () => {
@@ -185,16 +221,28 @@ describe('PCString.toAlphaLower', () => {
 	});
 
 	it('should replace new lines', () => {
-		const string = "\nA\nB\n\nC\n\n\nD\n\n\n\n";
-		const newString = PCString.domainSafeString(string);
-		expect(newString).toBe("abcd");
+		expect.assertions(1);
+		const string = '\nA\nB\n\nC\n\n\nD\n\n\n\n';
+		const newString = PCString.toAlphaLower(string);
+
+		expect(newString).toBe('abcd');
 	});
 
 	it('should replace new "My Cool Domain"', () => {
-		const string = "My Cool Domain";
-		const newString = PCString.domainSafeString(string);
-		expect(newString).toBe("mycooldomain");
+		expect.assertions(1);
+		const string = 'My Cool Domain';
+		const newString = PCString.toAlphaLower(string);
+
+		expect(newString).toBe('mycooldomain');
 	});
+
+	it('should leave hyphens alone', () => {
+		expect.assertions(1);
+		const string = 'My-Cool-Domain';
+		const newString = PCString.domainSafeString(string);
+
+		expect(newString).toBe('my-cool-domain');
+  });
 
 	it('should replace hyphens', () => {
 		const string = "My-Cool-Domain";
@@ -203,9 +251,153 @@ describe('PCString.toAlphaLower', () => {
 	});
 
 	it('should handle null', () => {
+		expect.assertions(1);
 		const string = null;
 		const newString = PCString.domainSafeString(string);
+
 		expect(newString).toBe('');
+	});
+});
+
+describe('test PCString.randomId', () => {
+	it('returns random string with no input', () => {
+		expect.assertions(1);
+		const results = PCString.randomId();
+
+		expect(results).toHaveLength(10);
+	});
+
+	it('returns random string with an input of 0', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(0);
+
+		expect(results).toHaveLength(10);
+	});
+
+	it('returns random string with an input of 1', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(1);
+
+		expect(results).toHaveLength(1);
+	});
+
+	it('returns random string with an input of 2', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(2);
+
+		expect(results).toHaveLength(2);
+	});
+
+	it('returns random string with an input of 3', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(3);
+
+		expect(results).toHaveLength(3);
+	});
+
+	it('returns random string with an input of 4', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(4);
+
+		expect(results).toHaveLength(4);
+	});
+
+	it('returns random string with an input of 5', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(5);
+
+		expect(results).toHaveLength(5);
+	});
+
+	it('returns random string with an input of 6', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(6);
+
+		expect(results).toHaveLength(6);
+	});
+
+	it('returns random string with an input of 7', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(7);
+
+		expect(results).toHaveLength(7);
+	});
+
+	it('returns random string with an input of 8', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(8);
+
+		expect(results).toHaveLength(8);
+	});
+
+	it('returns random string with an input of 9', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(9);
+
+		expect(results).toHaveLength(9);
+	});
+
+	it('returns random string with an input of 10', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(10);
+
+		expect(results).toHaveLength(10);
+	});
+
+	it('returns random string with an input of 11', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(11);
+
+		expect(results).toHaveLength(11);
+	});
+
+	it('returns random string with an input of 12', () => {
+		expect.assertions(1);
+		const results = PCString.randomId(12);
+
+		expect(results).toHaveLength(12);
+	});
+});
+
+describe('test PCString.randomChar', () => {
+	it('returns random string with no input', () => {
+		expect.assertions(2);
+		const results = PCString.randomChar();
+
+		expect(results).toHaveLength(1);
+		expect('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789').toContain(results);
+	});
+
+	it('returns random string with an input of c', () => {
+		expect.assertions(2);
+		const results = PCString.randomChar('c');
+
+		expect(results).toHaveLength(1);
+		expect(results).toBe(results);
+	});
+
+	it('returns random string with an input of mp', () => {
+		expect.assertions(2);
+		const results = PCString.randomChar('mp');
+
+		expect(results).toHaveLength(1);
+		expect('mp').toContain(results);
+	});
+});
+
+describe('test PCString.randomString', () => {
+	it('returns random string with no input', () => {
+		expect.assertions(1);
+		const results = PCString.randomString();
+
+		expect(results).toHaveLength(10);
+	});
+
+	it('returns random string with an input of 5', () => {
+		expect.assertions(1);
+		const results = PCString.randomString(5);
+
+		expect(results).toHaveLength(5);
 	});
 });
 
--- src/PCString.js
+++ src/PCString.js
@@ -1,37 +1,90 @@
-class PCString  {
-
-	constructor() {
-		//Empty Constructor
-	}
-
+class PCString {
 	static isString(o) {
-		if(o === null) return false;
-		return typeof o == "string" || (typeof o == "object" && o.constructor === String);
+		if (o === null) {
+			return false;
+		}
+
+		return typeof o == 'string' || (typeof o == 'object' && o.constructor === String);
 	}
 
 	static hasWhitespace(string) {
-		if(!string) return false;
+		if (!string) {
+			return false;
+		}
+
 		return string.indexOf(' ') >= 0;
 	}
 
 	static removeWhitespace(string) {
-		if(!string) return '';
+		if (!string) {
+			return '';
+		}
 		return string.replace(/\s+/g, '');
 	}
 
 	static domainSafeString(string) {
-		if(!string) return '';
-		return string.toLowerCase().replace(/[^a-z0-9\-]/g, '');
+		if (!string) {
+			return '';
+		}
+
+		return string.toLowerCase().replace(/[^a-z0-9-]/g, '');
 	}
 
-	static toAlphaLower(str) {
+	// this function makes sure that the first letter isn't a number
+	static randomId(input) {
+		let length;
+
+		if (input) {
+			length = input;
+		} else {
+			length = 10;
+		}
+
+		let text = PCString.randomChar('abcdefghijklmnopqrstuvwxyz');
+
+		for (let i = 0; i < length - 1; i++) {
+			text += PCString.randomChar('abcdefghijklmnopqrstuvwxyz0123456789');
+		}
+
+		return text;
+	}
+
+	static randomString(input) {
+		let length;
+		let text = '';
+
+		if (input) {
+			length = input;
+		} else {
+			length = 10;
+		}
+
+		for (let i = 0; i < length; i++) {
+			text += PCString.randomChar('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+		}
+
+		return text;
+	}
+
+	static randomChar(input) {
+		let characters;
+
+		if (input) {
+			characters = input;
+		} else {
+			characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		}
+
+		return characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+
+static toAlphaLower(str) {
 		if (!str) {
 			return '';
 		}
 
 		return str.toLowerCase().replace(/[^a-z]/g, '');
 	}
-
 }
 
 module.exports = PCString;
